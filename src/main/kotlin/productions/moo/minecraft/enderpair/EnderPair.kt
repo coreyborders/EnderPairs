f3574fb0dc7fff3561f72bc7c4157ecde0283550
coreyborders <corey.borders@gmail.com>
2021-10-08 20:06:56 -0400

    Began changing things from items to block entities

--- src/main/kotlin/productions/moo/minecraft/enderpair/EnderPair.kt
+++ src/main/kotlin/productions/moo/minecraft/enderpair/EnderPair.kt
@@ -1,19 +1,21 @@
 package productions.moo.minecraft.enderpair
 
 import net.fabricmc.api.ModInitializer
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings
-import net.minecraft.item.ItemGroup
+import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
+import net.minecraft.block.Material
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
 
 @Suppress("UNUSED")
-object EnderPair: ModInitializer {
+object EnderPair : ModInitializer {
     private const val MOD_ID = "ender_pair"
 
-    val PAIRED_CHEST_ITEM = PairedChestItem(FabricItemSettings().group(ItemGroup.MISC).maxCount(2))
+    val PAIRED_CHEST = Identifier(MOD_ID, "paired_chest")
+
+    val PAIRED_CHEST_BLOCK = PairedChestBlock(FabricBlockSettings.of(Material.METAL).requiresTool())
 
     override fun onInitialize() {
-        Registry.register(Registry.ITEM, Identifier(MOD_ID, "paired_chest"), PAIRED_CHEST_ITEM)
+        Registry.register(Registry.BLOCK, Identifier(MOD_ID, "paired_chest"), PAIRED_CHEST_BLOCK)
         println("Ender Pairs has been initialized.")
     }
 }
