2dc291385de466def77187c94173516590933dc1
Casey Borders <TheBeast.13@gmail.com>
2021-10-11 20:05:40 -0700

    Use a custom item to spawn blocks with a custom texture

--- src/main/kotlin/productions/moo/minecraft/enderpair/EnderPair.kt
+++ src/main/kotlin/productions/moo/minecraft/enderpair/EnderPair.kt
@@ -1,21 +1,22 @@
 package productions.moo.minecraft.enderpair
 
+import PairedChestItem
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.minecraft.block.Material
+import net.minecraft.item.ItemGroup
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
 
 @Suppress("UNUSED")
 object EnderPair : ModInitializer {
     private const val MOD_ID = "ender_pair"
-
-    val PAIRED_CHEST = Identifier(MOD_ID, "paired_chest")
-
-    val PAIRED_CHEST_BLOCK = PairedChestBlock(FabricBlockSettings.of(Material.METAL).requiresTool())
+    val PAIRED_CHEST_BLOCK = PairedChestBlock(FabricBlockSettings.of(Material.METAL).strength(4.0f).requiresTool())
+    val PAIRED_CHEST_ITEM = PairedChestItem(PAIRED_CHEST_BLOCK, FabricItemSettings().group(ItemGroup.MISC))
 
     override fun onInitialize() {
         Registry.register(Registry.BLOCK, Identifier(MOD_ID, "paired_chest"), PAIRED_CHEST_BLOCK)
-        println("Ender Pairs has been initialized.")
+        Registry.register(Registry.ITEM, Identifier(MOD_ID, "paired_chest"), PAIRED_CHEST_ITEM)
     }
 }
