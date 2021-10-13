81610f6c666b6601658876b1b876879958c636c7
Casey Borders <TheBeast.13@gmail.com>
2021-10-12 19:21:41 -0700

    Add files that were incorrectly ignored

---
+++ src/main/kotlin/productions/moo/minecraft/enderpair/PairedChestBlock.kt
@@ -0,0 +1,8 @@
+package productions.moo.minecraft.enderpair
+
+import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
+import net.minecraft.block.Block
+
+class PairedChestBlock(settings: FabricBlockSettings) : Block(settings) {
+
+}
