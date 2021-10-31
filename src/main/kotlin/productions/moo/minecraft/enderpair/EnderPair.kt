1b45957086fb1fc4108be5bdbdeb8913ef248975
Casey Borders <TheBeast.13@gmail.com>
2021-10-31 13:27:46 -0700

    Getting the chest to display in the hand

--- src/main/kotlin/productions/moo/minecraft/enderpair/EnderPair.kt
+++ src/main/kotlin/productions/moo/minecraft/enderpair/EnderPair.kt
@@ -6,7 +6,7 @@ import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
 
 object EnderPair : ModInitializer {
-    private const val MOD_ID = "ender_pair"
+    const val MOD_ID = "ender_pair"
     private val PAIRED_CHEST_IDENTIFIER: Identifier = Identifier(MOD_ID, "paired_chest")
     val PAIRED_CHEST_BLOCK = Registry.register(Registry.BLOCK, PAIRED_CHEST_IDENTIFIER, PairedChestBlock())!!
     val PAIRED_CHEST_ITEM = Registry.register(Registry.ITEM, PAIRED_CHEST_IDENTIFIER, PairedChestItem())!!
